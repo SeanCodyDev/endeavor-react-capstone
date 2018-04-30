6901aaf5ea81e3fec80c036d4efe77ffbac595d4
Stoic Dev <stoicwebdev@gmail.com>
2018-04-30 15:10:30 -0400

    before styled component undo

--- client/src/components/login-form.js
+++ client/src/components/login-form.js
@@ -3,6 +3,8 @@ import {Field, reduxForm, focus} from 'redux-form';
 import Input from './input';
 import {login} from '../actions/auth';
 import {required, nonEmpty} from '../validators';
+import styled from "styled-components";
+
 
 export class LoginForm extends React.Component {
     onSubmit(values) {
@@ -10,6 +12,13 @@ export class LoginForm extends React.Component {
     }
 
     render() {
+
+        const LoginCard = styled.div`
+            border-radius: 5px
+            background-color: white;
+            padding: 15px;
+        `;
+
         let error;
         if (this.props.error) {
             error = (
@@ -19,32 +28,34 @@ export class LoginForm extends React.Component {
             );
         }
         return (
-            <form
-                className="login-form"
-                onSubmit={this.props.handleSubmit(values =>
-                    this.onSubmit(values)
-                )}>
-                {error}
-                <label htmlFor="email">Email</label>
-                <Field
-                    component={Input}
-                    type="text"
-                    name="email"
-                    id="email"
-                    validate={[required, nonEmpty]}
-                />
-                <label htmlFor="password">Password</label>
-                <Field
-                    component={Input}
-                    type="password"
-                    name="password"
-                    id="password"
-                    validate={[required, nonEmpty]}
-                />
-                <button disabled={this.props.pristine || this.props.submitting}>
-                    Log in
-                </button>
-            </form>
+            <LoginCard>
+                <form
+                    className="login-form"
+                    onSubmit={this.props.handleSubmit(values =>
+                        this.onSubmit(values)
+                    )}>
+                    {error}
+                    <label htmlFor="email">Email</label>
+                    <Field
+                        component={Input}
+                        type="text"
+                        name="email"
+                        id="email"
+                        validate={[required, nonEmpty]}
+                    />
+                    <label htmlFor="password">Password</label>
+                    <Field
+                        component={Input}
+                        type="password"
+                        name="password"
+                        id="password"
+                        validate={[required, nonEmpty]}
+                    />
+                    <button disabled={this.props.pristine || this.props.submitting}>
+                        Log in
+                    </button>
+                </form>
+            </LoginCard>
         );
     }
 }
